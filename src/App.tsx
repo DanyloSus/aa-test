4d2dde67e63ae504b4ddde6f828a77c14c066d44
DanyloSus <danylo.sushko18@gmail.com>
2024-01-16 16:00:35 +0200

    Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import data from "./data.json";
 
 type CampaignType = {
@@ -33,61 +33,74 @@ type SortType =
   | "authB"
   | "dateB";
 
-const formatedData = data.map((account) => ({
-  ...account,
-  creationDate: new Date(account.creationDate),
-}));
-
 const App = () => {
+  const formatedData = data.map((account) => ({
+    ...account,
+    creationDate: new Date(account.creationDate),
+  }));
+
+  const pagesCount = Math.ceil(formatedData.length / 5);
+
+  const pageArray = Array.from({ length: pagesCount }, (_, index) => index + 1);
+
   const [sortBy, setSortBy] = useState<SortType>("id");
   const [sortedData, setSortedData] = useState<AccountType[]>(formatedData);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     let sortData;
     switch (sortBy) {
       case "id": {
-        sortData = formatedData.sort((a, b) => b.accountId - a.accountId);
+        sortData = formatedData.sort((a, b) => a.accountId - b.accountId);
+        console.log("id");
         break;
       }
       case "email": {
         sortData = formatedData.sort((a, b) => b.email.localeCompare(a.email));
+        console.log("email");
         break;
       }
       case "auth": {
         sortData = formatedData.sort((a, b) =>
           b.authToken.localeCompare(a.authToken)
         );
+        console.log("auth");
         break;
       }
       case "date": {
         sortData = formatedData.sort(
           (a, b) => b.creationDate.getDate() - a.creationDate.getDate()
         );
+        console.log("date");
         break;
       }
       case "idB": {
-        sortData = formatedData.sort((a, b) => a.accountId - b.accountId);
+        sortData = formatedData.sort((a, b) => b.accountId - a.accountId);
+        console.log("idB");
         break;
       }
       case "emailB": {
         sortData = formatedData.sort((a, b) => a.email.localeCompare(b.email));
+        console.log("emailB");
         break;
       }
       case "authB": {
         sortData = formatedData.sort((a, b) =>
           a.authToken.localeCompare(b.authToken)
         );
+        console.log("authB");
         break;
       }
       case "dateB": {
         sortData = formatedData.sort(
           (a, b) => a.creationDate.getDate() - b.creationDate.getDate()
         );
+        console.log("dateB");
         break;
       }
     }
     setSortedData(sortData);
-  }, [sortBy]);
+  }, [sortBy, page]);
 
   const handleChangeSort = (sort: SortType) => {
     if (sortBy === sort) {
@@ -118,57 +131,68 @@ const App = () => {
       />
     </svg>
   );
+  const pagination = (
+    <div>
+      {pageArray.map((pageNumber) => (
+        <p key={pageNumber} onClick={() => setPage(pageNumber)}>
+          Page: {pageNumber}
+        </p>
+      ))}
+    </div>
+  );
 
   return (
-    <div className="d-flex vw-100 vh-100 align-items-center justify-content-center">
-      <table className="container w-50-sm text-center">
-        <thead>
-          <tr className="row fw-bold">
-            <th
-              className="col d-flex align-items-center justify-content-center"
-              onClick={() => handleChangeSort("id")}
-            >
-              AccountId
-              {sortBy.includes("id") && arrow}
-            </th>
-            <th
-              className="col d-flex align-items-center justify-content-center"
-              onClick={() => handleChangeSort("email")}
-            >
-              Email
-              {sortBy.includes("email") && arrow}
-            </th>
-            <th
-              className="col d-flex align-items-center justify-content-center"
-              onClick={() => handleChangeSort("auth")}
-            >
-              AuthToken
-              {sortBy.includes("auth") && arrow}
-            </th>
-            <th
-              className="col d-flex align-items-center justify-content-center"
-              onClick={() => handleChangeSort("date")}
-            >
-              CreationDate
-              {sortBy.includes("date") && arrow}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedData.map((account) => (
-            <tr
-              className="row align-items-center py-1 "
-              key={account.accountId}
-            >
-              <td className="col">{account.accountId}</td>
-              <td className="col">{account.email}</td>
-              <td className="col">{account.authToken}</td>
-              <td className="col">{account.creationDate.toDateString()}</td>
+    sortedData && (
+      <div className="d-flex vw-100 vh-100 align-items-center justify-content-center">
+        <table className="container w-50-sm text-center">
+          <thead>
+            <tr className="row fw-bold py-1">
+              <th
+                className="col d-flex align-items-center justify-content-center"
+                onClick={() => handleChangeSort("id")}
+              >
+                AccountId
+                {sortBy.includes("id") && arrow}
+              </th>
+              <th
+                className="col d-flex align-items-center justify-content-center"
+                onClick={() => handleChangeSort("email")}
+              >
+                Email
+                {sortBy.includes("email") && arrow}
+              </th>
+              <th
+                className="col d-flex align-items-center justify-content-center"
+                onClick={() => handleChangeSort("auth")}
+              >
+                AuthToken
+                {sortBy.includes("auth") && arrow}
+              </th>
+              <th
+                className="col d-flex align-items-center justify-content-center"
+                onClick={() => handleChangeSort("date")}
+              >
+                CreationDate
+                {sortBy.includes("date") && arrow}
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {sortedData!
+              .slice((page - 1) * 5, page * 5)
+              .map((account, index) => (
+                <tr className="row align-items-center py-1 " key={index}>
+                  <td className="col">{account.accountId}</td>
+                  <td className="col">{account.email}</td>
+                  <td className="col">{account.authToken}</td>
+                  <td className="col">{account.creationDate.toDateString()}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        {pagination}
+      </div>
+    )
   );
 };
 
